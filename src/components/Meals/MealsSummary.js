f2bbac585bc868f0da1f6f611c1be31b873dbcf4
huntersteffner <huntersteffner@gmail.com>
2022-10-31 12:24:35 -0400

    Added Meals

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered to you</h2>
+      <p>
+        Choose your favorite meal from our broad selection of available options,
+        and enjoy a delicious lunch or dinner in your home.
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingredients by expert chefs.
+      </p>
+    </section>
+  )
+}
+
+export default MealsSummary
