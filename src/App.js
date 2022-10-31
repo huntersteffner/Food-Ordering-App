98ac23dd67c6ef785de755fb119b3ebf366405e6
huntersteffner <huntersteffner@gmail.com>
2022-10-31 16:34:49 -0400

    Added modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import {Fragment} from 'react'
+import { Fragment } from 'react'
 
-import Header from "./components/Layout/Header";
-import Meals from './components/Meals/Meals';
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+import Cart from './components/Cart/Cart'
 
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Cart />
+      <Header />
       <main>
-        <Meals/>
+        <Meals />
       </main>
     </Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
